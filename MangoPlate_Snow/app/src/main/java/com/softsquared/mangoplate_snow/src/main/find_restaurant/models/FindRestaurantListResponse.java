f24765a5721390dfe0dd190acfc01b407ed21d72
hyeni_Y <kwitys@naver.com>
2020-05-07 02:04:48 +0900

    맛집찾기탭 가게 목록 api 적용 성공

--- MangoPlate_Snow/app/src/main/java/com/softsquared/mangoplate_snow/src/main/find_restaurant/models/FindRestaurantListResponse.java
+++ MangoPlate_Snow/app/src/main/java/com/softsquared/mangoplate_snow/src/main/find_restaurant/models/FindRestaurantListResponse.java
@@ -1,87 +1,11 @@
 package com.softsquared.mangoplate_snow.src.main.find_restaurant.models;
 
 import com.google.gson.annotations.SerializedName;
+import com.softsquared.mangoplate_snow.src.main.find_restaurant.Restaurant;
+
+import java.util.ArrayList;
 
 public class FindRestaurantListResponse {
-    public class RestaurantListResult {
-
-        @SerializedName("areaId")
-        private int areaId;
-
-        @SerializedName("area")
-        private String area;
-
-        @SerializedName("restaurantId")
-        private int restaurantId;
-
-        @SerializedName("img")
-        private String img;
-
-        @SerializedName("star")
-        private String star;
-
-        @SerializedName("title")
-        private String title;
-
-        @SerializedName("distance")
-        private String distance;
-
-        @SerializedName("seenNum")
-        private String seenNum;
-
-        @SerializedName("reviewNum")
-        private int reviewNum;
-
-        @SerializedName("rating")
-        private float rating;
-
-        @SerializedName("ratingColor")
-        private String ratingColor;
-
-        public int getAreaId() {
-            return areaId;
-        }
-
-        public String getArea() {
-            return area;
-        }
-
-        public int getRestaurantId() {
-            return restaurantId;
-        }
-
-        public String getImg() {
-            return img;
-        }
-
-        public String getStar() {
-            return star;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getDistance() {
-            return distance;
-        }
-
-        public String getSeenNum() {
-            return seenNum;
-        }
-
-        public int getReviewNum() {
-            return reviewNum;
-        }
-
-        public float getRating() {
-            return rating;
-        }
-
-        public String getRatingColor() {
-            return ratingColor;
-        }
-    }
     @SerializedName("code")
     private int code;
 
@@ -92,7 +16,7 @@ public class FindRestaurantListResponse {
     private boolean isSuccess;
 
     @SerializedName("result")
-    private RestaurantListResult restaurantListResult;
+    private ArrayList<Restaurant> restaurantListResult;
 
     public int getCode(){
         return code;
@@ -108,7 +32,7 @@ public class FindRestaurantListResponse {
         return isSuccess;
     }
 
-    public RestaurantListResult getRestaurantListResult() {
+    public ArrayList<Restaurant> getRestaurantListResult() {
         return restaurantListResult;
     }
 }
